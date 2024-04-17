74caeca4e4dbb69dbf2b399e5e34c6e47fb4cd94
Mario <mariovidovic97@gmail.com>
2024-04-17 07:25:09 +0200

    filepond added

--- events/create-new.php
+++ events/create-new.php
@@ -12,6 +12,14 @@
         define('meta', TRUE);
         include '../inc/head.php';
     ?>
+    <!-- include FilePond library -->
+    <link href="https://unpkg.com/filepond/dist/filepond.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/filepond-plugin-file-encode/dist/filepond-plugin-file-encode.min.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.min.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.min.js"></script>
+    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js"></script>
+    <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
     <title>New event | EventWave</title>
 </head>
 <body>
@@ -26,9 +34,26 @@
                 <h3>Create new event</h3>
             </div>
             <div class="wrapper-content">
+                <input type="file" class="filepond event-header-image" name="filepond">
                 <h5>Event information</h5>
             </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            FilePond.registerPlugin(
+                // encodes the file as base64 data
+                FilePondPluginFileEncode,
+                // validates the size of the file
+                FilePondPluginFileValidateSize,
+                // corrects mobile image orientation
+                FilePondPluginImageExifOrientation,
+                // previews dropped images
+                FilePondPluginImagePreview
+            );
+            FilePond.create(document.querySelector('input.filepond'));
+        });
+    </script>
 </body>
 </html>
